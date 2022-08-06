6463118c02660f33ce676c3f81c1d415713034d1
Amal Abdullaev <amal.abdullaev.96@mail.ru>
2022-08-06 17:54:45 +0300

    commit 1

---
+++ src/Components/CarouselBox.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Carousel } from "bootstrap";
+import forestImg from '../assets/120.jpg';
+import dforestImg from '../assets/121.jpg';
+import rforestImg from '../assets/122.jpg';
+
+export default class CarouselBox extends Component {
+    render () {
+        return (
+           <Carousel>
+               <Carousel.Item>
+                   <img
+                      className="d-bloc w-100"
+                      src={ forestImg }
+                      alt="Forest"
+                   />
+                   <Carousel.Caption>
+                       <h3>JOB NOT WOLF</h3>
+                       <p>won't run into the forest</p>
+                   </Carousel.Caption>
+               </Carousel.Item>
+               <Carousel.Item>
+                   <img
+                      className="d-bloc w-100"
+                      src={ dforestImg }
+                      alt="Forest"
+                   />
+                   <Carousel.Caption>
+                       <h3>THIS IS A START</h3>
+                       <p>We will be fleas of space, jumping from star to star</p>
+                   </Carousel.Caption>
+               </Carousel.Item>
+               <Carousel.Item>
+                   <img
+                      className="d-bloc w-100"
+                      src={ rforestImg }
+                      alt="Forest"
+                   />
+                   <Carousel.Caption>
+                       <h3>88888</h3>
+                       <p>0000000000000000</p>
+                   </Carousel.Caption>
+               </Carousel.Item>
+           </Carousel>
+        )
+    }
+}
---
+++ src/Components/Header.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Navbar, Nav, FormControl, Container, Form, Button } from "react-bootstrap";
+import logo from './logo192.png';
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Contacts from "../Pages/Contacts";
+import Blog from "../Pages/Blog";
+
+export const Header = () => {
+    const navigate = useNavigate()
+    const goTo = (url) => {
+        navigate(url)
+    }
+
+    return (
+        <>
+            <Navbar fixed="top" CollapseOnSelect expand="md" bg="dark" variant="dark" >
+                <Container>
+                    <Navbar.Brand href="/" >
+                        <img
+                            src={logo}
+                            height="30"
+                            width="30"
+                            className="d-inline-bloc aline-top"
+                            alt="logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle arie-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav" >
+                        <Nav className="mr-auto">
+                            <Nav.Link onClick={() => goTo('/')}> Home </Nav.Link>
+                            <Nav.Link onClick={() => goTo('/')}> About us </Nav.Link>
+                            <Nav.Link onClick={() => goTo('/')}> Contact </Nav.Link>
+                            <Nav.Link onClick={() => goTo('/')}> Blog </Nav.Link>
+                        </Nav>
+                        <Form inline >
+                            style={"margin-left: auto"}
+                            <FormControl
+                                type="text"
+                                placeholder="Search"
+                                className="mr-sm-2"
+                            />
+                            <Button margin-left="auto" variant="outline-info">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <div style={{ marginTop: 110 }}>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/about" element={<About />} />
+                    <Route exact path="/contacts" element={<Contacts />} />
+                    <Route exact path="/blog" element={<Blog />} />
+                </Routes>
+            </div>
+        </>
+    )
+}
