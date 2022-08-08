cbdcd11992b104b0c19cff93e5fb281338887bd8
Amal Abdullaev <amal.abdullaev.96@mail.ru>
2022-08-09 02:34:09 +0300

    Contacts

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,11 +1,32 @@
+import { Button } from "bootstrap";
 import React, { Component } from "react";
+import { Container, Form } from "react-bootstrap";
 
 export default class Contacts extends Component {
-    render () {
+    render() {
         return (
-            <div>
-                Contacts
-            </div>
+            <Container style={{ width: '500px' }}>
+                <h className="text-center"> Contacts us</h>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text>
+                            We'll never share your email with  anyone else
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Exsample textarea</Form.Label>
+                        <Form.Control as="textarea" rows="3" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Check type="checkbox" label="Check me out"/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit" >Submit</Button>
+                </Form>
+            </Container>
         )
     }
 }
