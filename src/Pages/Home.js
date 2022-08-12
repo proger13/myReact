0d1a76a136fa8e18e23717e9e705b142dee0b540
Amal Abdullaev <amal.abdullaev.96@mail.ru>
2022-08-12 03:39:59 +0300

    home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import { Container, CardDeck, Card  } from "react-bootstrap";
 import CarouselBox from "../Components/CarouselBox";
 
 export default class Home extends Component {
     render () {
         return (
+            <>
          <CarouselBox/>
+         <Container>
+             <h2 className="text-center m-4"> OUR TEAM </h2>
+             <CardDeck>
+                 <Card>
+
+                 </Card>
+             </CardDeck>
+         </Container>
+         </>
         )
     }
 }
