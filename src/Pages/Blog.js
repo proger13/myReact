b74a6230b86a25edd1fec147f7ebbf655fbe9ce7
Amal Abdullaev <amal.abdullaev.96@mail.ru>
2022-08-16 23:38:52 +0300

    update

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,11 +1,98 @@
 import React, { Component } from "react";
+import { Container, Row, Col, ListGroup, Card } from "react-bootstrap";
+import { Media } from 'react-media';
 
 export default class Blog extends Component {
-    render () {
+    render() {
         return (
-            <div>
-                Blog
-            </div>
+            <Container>
+                <Row>
+                    <Col md="9">
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://images.squarespace-cdn.com/content/v1/58d20c79725e25b221549193/1521098258210-W9FBV72SMJJX3LESUA7Z/js.jpg?format=2500w"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>
+                                    Blog about the first website I wrote.
+                                    We are on the threshold of a long journey and soon everything will change.
+                                </p>
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://images.squarespace-cdn.com/content/v1/58d20c79725e25b221549193/1521098258210-W9FBV72SMJJX3LESUA7Z/js.jpg?format=2500w"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>
+                                    Blog about the first website I wrote.
+                                    We are on the threshold of a long journey and soon everything will change.
+                                </p>
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://images.squarespace-cdn.com/content/v1/58d20c79725e25b221549193/1521098258210-W9FBV72SMJJX3LESUA7Z/js.jpg?format=2500w"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>
+                                    Blog about the first website I wrote.
+                                    We are on the threshold of a long journey and soon everything will change.
+                                </p>
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://images.squarespace-cdn.com/content/v1/58d20c79725e25b221549193/1521098258210-W9FBV72SMJJX3LESUA7Z/js.jpg?format=2500w"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>
+                                    Blog about the first website I wrote.
+                                    We are on the threshold of a long journey and soon everything will change.
+                                </p>
+                            </Media.Body>
+                        </Media>
+                    </Col>
+                    <Col md="3">
+                        <h5>Categories</h5>
+                        <Card>
+                            <ListGroup>
+                                <ListGroup.Item>Java</ListGroup.Item>
+                                <ListGroup.Item>C++</ListGroup.Item>
+                                <ListGroup.Item>JS</ListGroup.Item>
+                                <ListGroup.Item>Python</ListGroup.Item>
+                                <ListGroup.Item>HTML/CSS</ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+
+                        <Card className="mt-3">
+                            <Card.Body>
+                                <Card.Title>Side widget</Card.Title>
+                                <Card.Text>
+                                    Blog about the first website I wrote.
+                                    We are on the threshold of a long journey and soon everything will change.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
